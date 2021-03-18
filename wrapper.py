2ac1687e1dd958081d45caea24e16063a9ecec6d
wijwij <6858881+wijwij@users.noreply.github.com>
2021-03-18 15:46:13 -0400

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,3 +1,3 @@
 import sys
 from nth_fibonacci import nth_fibonacci
-print nth_fibonacci(sys.argv[1]) # call the function on the first command line arg
+print(nth_fibonacci(sys.argv[1])) # call the function on the first command line arg
